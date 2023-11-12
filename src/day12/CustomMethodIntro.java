3f6e16059b7258a874a01ba23dda5d45a948d886
MelikTasdemir <mlktasdemir@gmail.com>
2023-11-12 17:41:32 +0100

    Initial commit

---
+++ src/day12/CustomMethodIntro.java
@@ -0,0 +1,36 @@
+package day12;
+
+public class CustomMethodIntro {
+    public static void main(String[] args) {
+
+
+        System.out.println("Test 1-2-3");
+
+great();
+
+        System.out.println("Test is okey");
+message();
+    }
+
+
+
+    public static void great() {
+        System.out.println("Hello");
+        System.out.println("Cydeo");
+        System.out.println("Are you ready");
+    }
+
+
+    public static void message(){
+
+        System.out.println("Heylo");
+
+
+
+    }
+
+
+
+
+
+}

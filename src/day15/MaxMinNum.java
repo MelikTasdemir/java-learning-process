eb2fc43125dd6f0f080f604e0eabda2da75c9e95
MelikTasdemir <mlktasdemir@gmail.com>
2023-11-22 11:50:35 +0100

    Initial commit

---
+++ src/day15/MaxMinNum.java
@@ -0,0 +1,28 @@
+package day15;
+
+import java.util.Scanner;
+
+public class MaxMinNum {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in) ;
+
+        int max = -2147483648 ;
+        int min = 2147483647 ;
+
+        for (int i = 0; i < 5 ; i++) {
+
+            System.out.println("Enter a number : ");
+
+            int num = input.nextInt() ;
+
+            if( num> max) {
+                max = num ;
+            }
+            if (num < min) min = num;
+        }
+
+        System.out.println("Max number is = " + max);
+        System.out.println("Min number is = " + min);
+    }
+}

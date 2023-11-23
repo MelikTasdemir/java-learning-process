a78b87fb5a02d9d733acda29141de59876c5955b
Melik Tasdemir <mlktasdemir@gmail.com>
2023-11-23 19:25:49 +0100

    Initial commit

---
+++ src/day16/Compare.java
@@ -0,0 +1,24 @@
+package day16;
+
+public class Compare {
+    public static void main(String[] args) {
+
+        int a = 0 ;
+        while (a < 5){
+            a++;
+            System.out.println("Hello " + a);
+
+        }
+        System.out.println("===================");
+
+        int k = 1 ;
+        do {
+            System.out.println("Hi" + k);
+            k ++ ;
+
+        } while (k < 5) ;
+
+
+
+    }
+}
